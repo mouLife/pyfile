67f30424e0810ce0f649d9475157bbec524a835e
mouLife <moulelin@163.com>
2020-01-29 12:56:50 +0800

    42

---
+++ pytho进阶/基础/42算术运算——魔法方法.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   42算术运算——魔法方法.py    
+@Contact :   moulelin@163.com
+@License :   (C)Copyright 2019 Moule
+@Modify Time      @Author  
+------------      -------    
+2020/1/29 12:56   MouleLin     
+'''    
+
+ 
