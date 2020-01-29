0080cadc44d305f788b68d48e2e70802efa5217a
mouLife <moulelin@163.com>
2020-01-29 12:55:49 +0800

    test

--- pytho进阶/基础/41构造和析构_魔法方法.py
+++ pytho进阶/基础/41构造和析构_魔法方法.py
@@ -8,5 +8,24 @@
 ------------      -------    
 2020/1/29 11:49   MouleLin     
 '''    
-
-print('12')
+# __new__方法 ，继承不可修改的对象的时候，重写这个方法
+class CapStr(str):
+    def __new__(cls, string):
+        '''
+        把所有字母转为大写字符
+        :param string:
+        '''
+        string = string.upper()
+        return str.__new__(cls,string)
+a = CapStr('asd')
+print(a)
+# __del__析构器 垃圾回收机制调用
+class C:
+    def __init__(self):
+        print('init method')
+    def __del__(self):
+        print("del method")
+c = C()
+c2 = c
+c3 = c2
+del c3
--- pytho进阶/基础/basic.py
+++ pytho进阶/基础/basic.py
@@ -5,4 +5,5 @@ a.extend(["skdjaskjdfbaskj","asdfsad"])# extend是用一个列表扩张另一个
 print(a)
 a.insert(1,"asd")
 # 字符串不可以更改
-print()
+a = 'ab'
+print(a.upper())
